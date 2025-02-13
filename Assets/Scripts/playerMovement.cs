77e7e7edb9123e61ff89701e72080f75eeb125e3
MangoDraco <le303029@ucf.edu>
2025-02-13 16:36:02 -0500

    simpleMovementCommit

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float speed = 3.0f;
+        transform.position += new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+
+    }
+}
